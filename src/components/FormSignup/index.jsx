16ddf5d1bf56b11e4acfd23acd0ed170cc7f0e09
lenghia148 <ledinhnghia.qb@gmail.com>
2022-07-31 22:33:49 +0700

    signup

--- src/components/FormSignup/index.jsx
+++ src/components/FormSignup/index.jsx
@@ -1,20 +1,25 @@
 import React,{useState,useEffect} from "react";
 import styles from "./FormSignup.module.css";
 import axios from "axios";
+
 const FormSignup = () => {
-    const [user,setUser] = useState('')
+    const [user,setUser] = useState({})
+    const [name,setName] = useState('')
     const [password,setPassword] = useState('')
+    const [mail,setMail] = useState('')
+    const [phoneNumer,setPhoneNumer] = useState('')
+    const [birthDay,setBirthDay] = useState('')
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [database, setDatabase] = useState([]);
-    const [users, setUsers] = useState([]);
+    
     const getData = async () => {
     setLoading(true);
     try {
       const res = await axios.get(
-        "https://login-c1022-default-rtdb.asia-southeast1.firebasedatabase.app/user.json"
+        "https://cuoikhoa-eedb4-default-rtdb.asia-southeast1.firebasedatabase.app/user.json"
       );
-      // console.log(Object.values(res.data));
+     
       setLoading(false);
       setDatabase(Object.values(res.data));
     } catch (error) {
@@ -24,12 +29,18 @@ const FormSignup = () => {
   useEffect(() => {
     getData();
   }, []);
-  const handlerSubmitRe = async (e) => {
+  const handlerSubmitRe =  async (e) => {
     e.preventDefault();
+    setUser({
+      name,
+      phoneNumer,
+      password,
+      mail,
+      birthDay
+    })
     try {
       await axios.post(
-        "https://login-c1022-default-rtdb.asia-southeast1.firebasedatabase.app/user.json",
-        users
+        "https://cuoikhoa-eedb4-default-rtdb.asia-southeast1.firebasedatabase.app/user.json",user
       );
       getData();
       setLoading(false);
@@ -37,36 +48,49 @@ const FormSignup = () => {
       setError(true);
     }
   };
+  
   return (
+    
     <div className={styles.container}>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       <div><img src="https://cdn.pizzahut.vn/images/Web_V3/Member/3054x201.jpg" alt="" /></div>
       <h1>🍕🍕 TÍCH ĐIỂM ĐỔI PIZZA NÀO!</h1>
       <h3>THAM GIA HUT REWARDS
       VÀ BẮT ĐẦU HÀNH TRÌNH TÍCH ĐIỂM CỦA BẠN!</h3>
-      <form action="" className={styles.form}>
-        <div className={styles.form_right}>
+    <form action="" className={styles.form}>
+      <div className={styles.form_right}>
         <label htmlFor="">Tên *</label>
-        <input type="text" value={user} onChange={e=>setUser(e.target.value)}/> 
+        <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/> 
         <label htmlFor="">Ngày sinh *</label>
-        <input type="date" name="" id="" />
-        <label htmlFor="">Giới tính *</label>
-        <input type="text" />
+        <input type="date" name=""  value={birthDay} onChange={(e)=>setBirthDay(e.target.value)}/>
         <label htmlFor="">Số điện thoại *</label>
-        <input type="number" name="" id="" />
+        <input type="number" name=""   value={phoneNumer} onChange={(e)=>setPhoneNumer(e.target.value)}/>
         <label htmlFor="">Địa chỉ Email *</label>
-        <input type="email" name="" id="" />
-        <input type="checkbox" name="" id="" /><span>Tôi đồng ý trở thành Thành viên Hut Rewards và chấp nhận các Điều khoản & Điều kiện và Chính sách bảo mật của Pizza Hut.</span>
-
-        </div>
+        <input type='email' name=""  value={mail} onChange={(e)=>setMail(e.target.value)}/>
+      <div className={styles.confirm_one}>
+        <input type="checkbox" className={styles.confirm_checkbox} name=""  />
+        <div>Tôi đồng ý trở thành Thành viên Hut Rewards và chấp nhận các Điều khoản & Điều kiện và Chính sách bảo mật của Pizza Hut.</div>
+      </div>
+      </div>
         <div className={styles.form_left}>
           <label htmlFor="">Mật khẩu *</label>
-          <input type="password" />
+          <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
           <label htmlFor="">Xác nhận mật khẩu *</label>
           <input type="password" />
+          <div>Mật khẩu ít nhất 8 ký tự và đáp ứng 4 điều kiện sau:</div>
+          <div>Các số 0-9. Ví dụ: 2, 6, 7</div>
+          <div>Các chữ cái thường (nhỏ) a-z. Ví dụ: a, e, r</div>
+          <div>Chữ cái viết hoa (in hoa) A-Z. Ví dụ: A, E, R</div>
+          <div>Các ký tự đặc biệt như !@#$</div>
+          <div className={styles.confirm_one}>
+        <input type="checkbox" className={styles.confirm_checkbox} name="" />
+        <div>
+Tôi đồng ý trở thành Thành viên Hut Rewards và chấp nhận các Điều khoản & Điều kiện và Chính sách bảo mật của Pizza Hut.</div>
+      </div>
         </div>
   
       </form>
-      <button >ĐĂNG KÝ</button>
+      <button onClick={handlerSubmitRe}>ĐĂNG KÝ</button>
     </div>
   );
 };
